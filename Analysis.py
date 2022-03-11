2b3d1ab2edafc0a3e8eb426b2b6684f7ae3dc4dd
johnz <johnzhanguoft@gmail.com>
2022-03-11 15:20:02 -0500

    Upload

---
+++ Analysis.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+from uncertainties import unumpy
+from uncertainties import ufloat
+import matplotlib.pyplot as plt
+import seaborn as sns
+from os import listdir
+from scipy import stats
+from scipy.optimize import curve_fit
+
+# data = pd.read_csv('Oil.txt', header=0)
+#
+# velocity = np.array(data['velocity(m/s)'].tolist())
+# velocity_error = np.array(data['standard_error'].tolist())
+# diameter = np.array(data['diameter'].tolist())
+#
+# # add errors
+# velocity = unumpy.uarray(velocity, velocity_error)
+# diameter = unumpy.uarray(diameter, 0.1)
+#
+# C = 1 - 2.104 * (diameter / ufloat(93.8, 0.1)) \
+#     + 2.089 * ((diameter / ufloat(93.8, 0.1)) ** 2)
+#
+# corrected_velocity = velocity / C
+# print(corrected_velocity)
+#
+# # diameters are currently in mm
+# diameter = diameter * 0.001
+#
+# density = 1.26 * 1000  # in kg/m^3
+# viscosity = 0.934  # in kg/m*s
+#
+# reynolds = density * (diameter/6) * velocity / viscosity
+#
+# radius = diameter/2
+#
+# plt.scatter(x=unumpy.nominal_values(radius), y=unumpy.nominal_values(velocity), color='blue')
+# plt.title('Terminal Velocity vs Bead Radius for Glycerine')
+# plt.xlabel('Bead Radius (m)')
+# plt.ylabel('Terminal Velocity (m/s)')
+# plt.show()
+
+
+data = pd.read_csv('Oil_no_outliers.txt', header=0)
+
+velocity = np.array(data['velocity(m/s)'].tolist())
+velocity_error = np.array(data['standard_error'].tolist())
+diameter = np.array(data['diameter'].tolist())
+
+# add errors
+velocity = unumpy.uarray(velocity, velocity_error)
+diameter = unumpy.uarray(diameter, 0.1)
+
+C = 1 - 2.104 * (diameter / ufloat(93.8, 0.1)) \
+    + 2.089 * ((diameter / ufloat(93.8, 0.1)) ** 2)
+
+corrected_velocity = velocity / C
+
+# diameters are currently in mm
+diameter = diameter * 0.001
+
+density = 1.26 * 1000  # in kg/m^3
+viscosity = 0.934  # in kg/m*s
+
+reynolds = density * (diameter / 6) * velocity / viscosity
+print(reynolds)
+
+radius = diameter / 2
+
+plt.errorbar(x=unumpy.nominal_values(radius), y=unumpy.nominal_values(velocity), yerr=unumpy.std_devs(velocity),
+             xerr=unumpy.std_devs(radius), fmt='bo', ecolor='black', linestyle='None')
+
+plt.title('Terminal Velocity vs Bead Radius for Glycerine')
+plt.xlabel('Bead Radius (m)')
+plt.ylabel('Terminal Velocity (m/s)')
+
+# Fit curve
+def calculate_velocity(r, a):
+    return a * (r ** 2)
+
+
+popt, pcov = curve_fit(calculate_velocity, unumpy.nominal_values(radius), unumpy.nominal_values(velocity))
+print(popt)
+print(np.sqrt(np.diag(pcov)))
+t = np.arange(0, 0.0035, 0.0001)
+plt.plot(t, calculate_velocity(t, popt))
+
+plt.show()
---
+++ main.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+from uncertainties import unumpy
+from uncertainties import ufloat
+import matplotlib.pyplot as plt
+import seaborn as sns
+from os import listdir
+from scipy import stats
+
+# files = [f for f in listdir('C:\\Users\\johnz\\PycharmProjects\\Motion in Fluids\\Data')]
+
+
+# Import data
+
+# for file in files:
+#     data = pd.read_csv(f'Data/{file}', sep='\t', header=1)
+#     sns.lineplot(x=data['Time(sec)'], y=data['Position(mm)'], color='blue', alpha=0.4)
+#     plt.title(file)
+#     plt.show()
+#
+#     start = float(input("Start time"))
+#     stop = float(input("Stop time"))
+#
+#     terminal_velocity = data.loc[(data['Time(sec)'] >= start) & (data['Time(sec)'] <= stop)]
+#
+#     terminal_velocity.to_csv(f'Terminal_Velocities/{file}')
+
+    # data['Position(mm)'] = data['Position(mm)'] * 0.001     # convert to meters
+    # position = np.array(data['Position(mm)'].tolist())
+    # time = np.array(data['Time(sec)'].tolist())
+
+
+# files = [f for f in listdir('C:\\Users\\johnz\\PycharmProjects\\Motion in Fluids\\Terminal_Velocities')]
+#
+# for file in files:
+#     data = pd.read_csv(f'Terminal_Velocities/{file}', sep=',', header=0)
+#     # sns.lineplot(x=data['Time(sec)'], y=data['Position(mm)'], color='blue', alpha=0.4)
+#     # plt.title(file)
+#     # plt.show()
+#
+#     data['Position(mm)'] = data['Position(mm)'] * 0.001     # convert to meters
+#     position = np.array(data['Position(mm)'].tolist())
+#     time = np.array(data['Time(sec)'].tolist())
+#
+#     slope, intercept, r, p, se = stats.linregress(time, position)
+#     print(str(slope) + ',' + str(r ** 2) + ',' + str(se))
+#
+
+#
+# terminal_velocity_df = pd.read_csv('Cleaned_Terminal_Velocities.txt', header=0)
+# # terminal_velocity_df['velocity_error(m/s)'] = 0
+#
+# for ind in terminal_velocity_df.index:
+#
+#     velocity = ufloat(float(terminal_velocity_df['velocity(m/s)'][ind]), float(terminal_velocity_df['standard_error'][ind]))
+#
+#     C = 1 - 2.104 * (ufloat(terminal_velocity_df['diameter'][ind], 0.1)/ufloat(93.8, 0.1)) \
+#         + 2.089 * ((ufloat(terminal_velocity_df['diameter'][ind], 0.1)/ufloat(93.8, 0.1)) ** 2)
+#
+#     corrected_velocity = velocity / C
+#
+#     print(corrected_velocity)
+
+
+    #
+    # terminal_velocity_df.loc['velocity(m/s)', ind] = corrected_velocity.n
+    # terminal_velocity_df.loc['velocity_error(m/s)', ind] = corrected_velocity.s
+
+# terminal_velocity_df.to_csv('Corrected_Velocities.csv')
+
+
